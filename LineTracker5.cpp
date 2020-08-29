240984489c9aa79aa97b01ba3e533a3e391a145b
yanko <ianperina@gmail.com>
2020-08-29 11:40:23 +0100

    Update LineTracker5.cpp

--- LineTracker5.cpp
+++ LineTracker5.cpp
@@ -1,9 +1,3 @@
-/*
-  Morse.cpp - Library for flashing Morse code.
-  Created by David A. Mellis, November 2, 2007.
-  Released into the public domain.
-*/
-
 #include "Arduino.h"
 #include "LineTracker5.h"
 
